a8627f51a89553dccd1aa83214161c7708937291
Jason Gerard <jasongerard321@gmail.com>
2023-12-01 18:19:23 -0500

    day1part1 getting started with Elixir

---
+++ advent-of-code-2023/day1part1.exs
@@ -0,0 +1,12 @@
+{:ok, content} = File.read("inputs/day1part1.txt")
+lines = content |> String.split("\n", trim: true)
+
+count = lines 
+        |> Enum.map(fn line -> line
+                               |> String.replace(~r/[^\d]/, "")
+                               |> String.split("", trim: true) end)
+        |> Enum.map(fn numbers -> "#{hd(numbers)}#{List.last(numbers)}" end)
+        |> Enum.map(&Integer.parse(&1) |> elem(0))
+        |> Enum.sum
+
+IO.inspect count
