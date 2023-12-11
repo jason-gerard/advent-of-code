03cada468fd1090fdf2330de6d67ccd73d64be39
Jason Gerard <jasongerard321@gmail.com>
2023-12-11 11:07:53 -0500

    Day11Part1

---
+++ advent-of-code-2023/day11part1.exs
@@ -0,0 +1,27 @@
+{:ok, content} = File.read("inputs/day11.txt")
+
+points = content |> String.split("\n", trim: true) |> Enum.map(&String.split(&1, "", trim: true))
+IO.inspect(points, charlists: :as_lists)
+
+row_expanded = points |> Enum.flat_map(fn row -> if Enum.all?(row, &(&1 == ".")), do: [row, row], else: [row] end)
+
+expanded_points = row_expanded
+|> Enum.zip_with(&Function.identity/1)
+|> Enum.flat_map(fn row -> if Enum.all?(row, &(&1 == ".")), do: [row, row], else: [row] end)
+|> Enum.zip_with(&Function.identity/1)
+IO.inspect(expanded_points, charlists: :as_lists)
+
+galaxies = expanded_points
+|> Enum.with_index
+|> Enum.flat_map(fn {row, i} ->
+  row |> Enum.with_index |> Enum.flat_map(fn {point, j} -> if point == "#", do: [{i, j}], else: [] end)
+end)
+pairs = galaxies |> Enum.flat_map(fn p1 -> galaxies |> Enum.flat_map(fn p2 -> if p1 != p2 && p1 > p2, do: [{p1, p2}], else: [] end) end)
+IO.inspect(pairs, charlists: :as_lists)
+
+# Since we are working on a manhattan grid we can just take the abs value between components
+distances = pairs
+|> Enum.map(fn {{i1, j1}, {i2, j2}} -> abs(i1 - i2) + abs(j1 - j2) end)
+|> Enum.sum
+
+IO.inspect(distances, charlists: :as_lists)
